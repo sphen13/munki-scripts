aa3b1fff3b0b3ef2ce22e989dd929bc3d131a424
sphen13 <sboyle@trmacs.com>
2017-10-18 21:22:35 -0400

    Fix script description line

--- Zoom Inactivity Quit/payload/Library/Scripts/ZoomInactivityQuit.sh
+++ Zoom Inactivity Quit/payload/Library/Scripts/ZoomInactivityQuit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#This script will monitor the computer for idle time over 15 minutes and auto logout the Guest user.
+#This script will monitor the computer for idle time over 15 minutes and if no Zoom meeting is in progress will quit Zoom.
 
 idl=$"`ioreg -c IOHIDSystem | awk '/HIDIdleTime/ {print int($NF/1000000000); exit}'`"
 idletime="900"
