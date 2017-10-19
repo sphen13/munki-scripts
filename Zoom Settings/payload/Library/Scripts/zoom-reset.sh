37ec22a69ee253337e681253778c2f5b2f825195
sphen13 <sboyle@trmacs.com>
2017-10-18 21:22:20 -0400

    Removal of debug logging

--- Zoom Settings/payload/Library/Scripts/zoom-reset.sh
+++ Zoom Settings/payload/Library/Scripts/zoom-reset.sh
@@ -2,10 +2,7 @@
 
 isZoomRunning=`ps -ax | grep [z]oom.us`
 
-#echo "zoom-reset is being run..." >> /var/log/zoom-reset.log
-
 if [ -z "$isZoomRunning" ]; then
-	echo "purging zoom settings..." >> /var/log/zoom-reset.log
 	rm -rf /Users/source/Library/Application\ Support/zoom.us/data/*
 	cp /Library/Application\ Support/zoom.us/data/zoomus.db /Users/source/Library/Application\ Support/zoom.us/data/
 fi
