175545aabc487aa481953d23b029a781c148c60f
sphen13 <sboyle@trmacs.com>
2017-11-28 21:22:51 -0500

    block_root_accunt_login.sh modifications
    better logic.  test for shadow hash.  allow script to run continuously
    to make sure it stays disabled.

--- Solarwinds RMM - GruntWork/Script Checks/README.md
+++ Solarwinds RMM - GruntWork/Script Checks/README.md
@@ -2,7 +2,13 @@
 
 Designed to be used as DSC or 24/7 script checks in RMM.  Mostly workarounds :/
 
-### purge_sus.sh
+### block_root_accunt_login.sh
+
+Patches security flaw in 10.13 which allows any user to login as root with no password.  This is designed to run as a 24x7 RMM script check.  It will ensure that the root user has a valid password set and that its ability to login is disabled.
+
+This does not have to be specific to RMM.
+
+## purge_sus.sh
 
 if we are running less than 10.12.6 and have not already run this script previously, fix permissions on /tmp and purge sus cache.
 
--- Solarwinds RMM - GruntWork/Script Checks/block_root_accunt_login.sh
+++ Solarwinds RMM - GruntWork/Script Checks/block_root_accunt_login.sh
@@ -1,47 +1,51 @@
 #!/bin/bash
 
 majorVer=`sw_vers -productVersion | cut -d'.' -f2`
+
+# Only act on 10.13
 if [ "$majorVer" -eq "13" ]; then
-  if [ ! -e /Library/Managed\ Installs/.root_disabled ]; then
+
+  # Test to see if the root password has been set
+  isPasswordSet=$(/usr/bin/dscl . -read /Users/root ShadowHashData 2>/dev/null)
+
+  if [[ -z "$isPasswordSet" ]]; then
     # Set root password to randomized 32 character long password
 
     rootpassword=$(openssl rand -base64 32)
 
+    echo "Setting root password to randomized 32 character long password"
     /usr/bin/dscl . -passwd /Users/root "$rootpassword"
-
-    # Disable root login by setting root's shell to /usr/bin/false.
-    # The original UserShell value is as follows:
-    #
-    # /bin/sh
-    #
-    # To revert it back to /bin/sh, run the following command:
-    # /usr/bin/dscl . -change /Users/root UserShell /usr/bin/false /bin/sh
-
-    rootshell=$(/usr/bin/dscl . -read /Users/root UserShell | awk '{print $2}')
-
-    if [[ -z "$rootshell" ]]; then
-
-       # If root shell is blank or otherwise not set,
-       # use dscl to set /usr/bin/false as the shell.
-
-       echo "Setting blank root shell to /usr/bin/false"
-       /usr/bin/dscl . -create /Users/root UserShell /usr/bin/false
-    else
-      if [ "$rootshell" != "/usr/bin/false" ]; then
-         # If root shell is set to an existing value, use dscl
-         # to change the shell from the existing value and set
-         # /usr/bin/false as the shell.
-
-         echo "Changing root shell from $rootshell to /usr/bin/false"
-         /usr/bin/dscl . -change /Users/root UserShell "$rootshell" /usr/bin/false
-       else
-         echo "Root disabled"
-       fi
-    fi
-    touch /Library/Managed\ Installs/.root_disabled
-  else
-    echo "Root disabled"
   fi
+
+  # Disable root login by setting root's shell to /usr/bin/false.
+  # The original UserShell value is as follows:
+  #
+  # /bin/sh
+  #
+  # To revert it back to /bin/sh, run the following command:
+  # /usr/bin/dscl . -change /Users/root UserShell /usr/bin/false /bin/sh
+
+  rootshell=$(/usr/bin/dscl . -read /Users/root UserShell | awk '{print $2}')
+
+  if [[ -z "$rootshell" ]]; then
+
+     # If root shell is blank or otherwise not set,
+     # use dscl to set /usr/bin/false as the shell.
+
+     echo "Setting blank root shell to /usr/bin/false"
+     /usr/bin/dscl . -create /Users/root UserShell /usr/bin/false
+  else
+    if [ "$rootshell" != "/usr/bin/false" ]; then
+       # If root shell is set to an existing value, use dscl
+       # to change the shell from the existing value and set
+       # /usr/bin/false as the shell.
+
+       echo "Changing root shell from $rootshell to /usr/bin/false"
+       /usr/bin/dscl . -change /Users/root UserShell "$rootshell" /usr/bin/false
+     fi
+  fi
+  
+  echo "Root disabled"
 fi
 
 exit 0
