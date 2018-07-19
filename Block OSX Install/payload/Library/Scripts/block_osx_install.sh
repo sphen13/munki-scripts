9405eddbb057dd9e9e3ed1cbd939d588890cb289
sphen13 <sboyle@trmacs.com>
2018-07-19 10:53:45 -0400

    Update block_osx_install for Mojave

--- Block OSX Install/payload/Library/Scripts/block_osx_install.sh
+++ Block OSX Install/payload/Library/Scripts/block_osx_install.sh
@@ -3,6 +3,6 @@
 #loggedinuser=`stat -f%Su /dev/console`
 
 killall InstallAssistant
-rm -rf "/Applications/Install macOS High Sierra Beta.app"
-rm -rf "/Applications/Install macOS High Sierra.app"
+rm -rf "/Applications/Install macOS Mojave Beta.app"
+rm -rf "/Applications/Install macOS Mojave.app"
 #sudo -u "$loggedinuser" osascript -e 'tell app "System Events" to display alert "Upgrading your Operating System without approval violates our policy"'
